820f16f68e3da04720b89eaee6448115f9065fcb
jdclzl <1272717213@qq.com>
2024-12-21 21:46:14 +0800

    baike

--- run.py
+++ run.py
@@ -4,6 +4,34 @@ from wxcloudrun import app
 import base64
 from aip import AipImageClassify
 
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
+
+def query(content):
+    # 请求地址
+    url = 'https://baike.baidu.com/item/' + urllib.parse.quote(content)
+    # 请求头部
+    headers = { 
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36' 
+    }
+    # 利用请求地址和请求头部构造请求对象
+    req = urllib.request.Request(url=url, headers=headers, method='GET')
+    # 发送请求，获得响应
+    response = urllib.request.urlopen(req)
+    # 读取响应，获得文本
+    text = response.read().decode('utf-8')
+    baike_info = {
+        "description": "",
+        "image_url": ""
+    }
+    soup = BeautifulSoup(text, 'html.parser')
+    meta_tag = soup.find('meta', attrs={'name': 'description'})
+    baike_info['description'] = meta_tag['content']
+    meta_tag = soup.find('meta', attrs={'name': 'image'})
+    baike_info['image_url'] = meta_tag['content']
+    return baike_info
+
 """ 你的 APPID AK SK """
 APP_ID = "56511544"
 API_KEY = "P7VEk6Dz8k9yPtYcp7cjKkJh"
@@ -21,6 +49,8 @@ def detect():
         img = request.json.get('img')
         decoded_data = base64.b64decode(img)
         res = client.plantDetect(decoded_data, options)
+        if 'description' not in res.result[0].baikeinfo:
+            res.result[0].baikeinfo = query(res.result[0].name)
         return res
     return {}
 
